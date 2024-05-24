fa776d5d2e2fabf38cb111d0a534a2de8a777ff1
Shahin4292 <shahinsarker878@gmail.com>
2024-05-24 13:10:40 +0600

    3. When the button is pressed, navigate to a new screen that displays a message Done

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -30,6 +30,21 @@ class _HomeScreenState extends State<HomeScreen> {
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
+                ScaffoldMessenger.of(context).clearMaterialBanners();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text("Let's go to New Screen"),
+                    duration: Duration(seconds: 1),
+                  ),
+                );
+                Future.delayed(const Duration(seconds: 0), () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const NewScreen(message: 'Hello from the Home Screen!'),
+                    ),
+                  );
+                });
               },
               child: const Text('Press Me',style:  TextStyle(fontSize: 18),),
             ),
@@ -40,4 +55,24 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
+class NewScreen extends StatelessWidget {
+  final String message;
+
+  const NewScreen({super.key, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Screen'),
+      ),
+      body: Center(
+        child: Text(
+          message,
+          style: const TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
 
